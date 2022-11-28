8d6a7d0694d3051447c1ef29cca3a665d8c07b58
ljf (zamentur) <zamentur@users.noreply.github.com>
2022-11-28 22:01:51 +0100

    [fix] Wording

--- anchors.php
+++ anchors.php
@@ -66,8 +66,8 @@ class AnchorsPlugin extends Plugin
             $icon = $this->config->get('plugins.anchors.icon') ? "icon: '{$this->config->get('plugins.anchors.icon')}'," : '';
             $class = $this->config->get('plugins.anchors.class') ? "class: '{$this->config->get('plugins.anchors.class')}'," : '';
             $truncate = "truncate: {$this->config->get('plugins.anchors.truncate', 64)},";
-            $ariaLabel = "ariaLabel: '{$this->config->get('plugins.anchors.aria', 'Copy the link to the section: %s')}',";
-            $titleText = "titleText: '{$this->config->get('plugins.anchors.title', 'Copy the link to this section')}',";
+            $ariaLabel = "ariaLabel: '{$this->config->get('plugins.anchors.aria', 'Copy the link and go to this section')}',";
+            $titleText = "titleText: '{$this->config->get('plugins.anchors.title', 'Copy the link and go to this section')}',";
             $this->grav['assets']->addJs('plugin://anchors/js/anchor.min.js');
 
             $anchors_init =
